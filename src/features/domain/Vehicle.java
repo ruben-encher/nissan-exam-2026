dd8cc6c3fe489aa8b67d5dfc289209c4abc1de48
EHR <ruben-encher@educa.jcyl.es>
2026-03-05 17:59:51 +0100

    Modelar vehículo

---
+++ src/features/domain/Vehicle.java
@@ -0,0 +1,38 @@
+package features.domain;
+
+public class Vehicle {
+
+    private int id;
+    private String marca;
+    private String model;
+    private int capacity;
+    private String type;
+
+    public Vehicle(int id, String model, String marca, int capacity, String type) {
+        this.id = id;
+        this.model = model;
+        this.marca = marca;
+        this.capacity = capacity;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+}
