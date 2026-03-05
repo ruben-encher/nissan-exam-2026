9748f712a60e1ba521f987476588ef19161ccd5f
EHR <ruben-encher@educa.jcyl.es>
2026-03-05 18:25:26 +0100

    Task 4

--- src/features/domain/GetVehicleUseCase.java
+++ src/features/domain/GetVehicleUseCase.java
@@ -1,4 +1,14 @@
 package features.domain;
 
+import java.util.ArrayList;
+
 public class GetVehicleUseCase {
+
+    private VehicleRepository vehicleRepository;
+
+    public GetVehicleUseCase(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    ArrayList<Vehicle> storage = new ArrayList<>();
 }
